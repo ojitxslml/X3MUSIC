acc0cf13d6ab8e0fb6e5036ed67ae49cc72180cf
0x5 <jp.urra42@gmail.com>
2022-03-27 13:26:34 -0300

    Login

---
+++ js/register.js
@@ -0,0 +1,27 @@
+console.log("hola");
+
+// Get the modal
+const modal = document.getElementById("modalLogin");
+
+// Get the button that opens the modal
+const btn = document.getElementById("btnLogin");
+
+const btnIniciar = document.getElementById("btnIniciar");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log("hola");
+}
+
+btnIniciar.onclick = () =>{
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  console.log(event);
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
